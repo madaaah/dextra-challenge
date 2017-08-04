d35e5ae245f3fa91d7db01f24fb190b0342e9dcd
Madalena Benndorf <madalena.ben@gmail.com>
2017-08-04 17:18:40 -0300

    Making it look presentable

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 var connect = require('gulp-connect');
  
 gulp.task('sass', function () {
-  return gulp.src('src/stylesheets/*.scss')
+  return gulp.src('src/sass/*.scss')
     .pipe(sass({ errLogToConsole: true }))
     .pipe(gulp.dest('./css/'));
 });
@@ -25,7 +25,7 @@ gulp.task('livereload', function (){
 gulp.task('watch', function () {
   gulp.watch('./*', ['livereload']);
   gulp.watch('./css/*', ['livereload']);
-  gulp.watch('./src/stylesheets/*.scss', ['sass']);
+  gulp.watch('./src/sass/*.scss', ['sass']);
 });
 
 gulp.task('default', ['connect', 'watch', 'sass']);
